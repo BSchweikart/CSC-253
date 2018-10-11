a3f9cad2df232b8fcba41618f49df53aa9129fcf
Brian Schweikart <31232164+BSchweikart@users.noreply.github.com>
2018-10-10 21:56:23 -0400

    Add files via upload

---
+++ M2HW3_Schweikart/M2HW3_Schweikart/Name Search/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Name_Search
+{
+    partial class nameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.displayButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.boyTextBox = new System.Windows.Forms.TextBox();
+            this.girlTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // displayButton
+            // 
+            this.displayButton.Location = new System.Drawing.Point(15, 226);
+            this.displayButton.Name = "displayButton";
+            this.displayButton.Size = new System.Drawing.Size(75, 53);
+            this.displayButton.TabIndex = 0;
+            this.displayButton.Text = "Display Names";
+            this.displayButton.UseVisualStyleBackColor = true;
+            this.displayButton.Click += new System.EventHandler(this.displayButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(219, 226);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 53);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Please enter a Boy and A Girl name: ";
+            // 
+            // boyTextBox
+            // 
+            this.boyTextBox.Location = new System.Drawing.Point(128, 80);
+            this.boyTextBox.Name = "boyTextBox";
+            this.boyTextBox.Size = new System.Drawing.Size(166, 22);
+            this.boyTextBox.TabIndex = 3;
+            // 
+            // girlTextBox
+            // 
+            this.girlTextBox.Location = new System.Drawing.Point(128, 140);
+            this.girlTextBox.Name = "girlTextBox";
+            this.girlTextBox.Size = new System.Drawing.Size(166, 22);
+            this.girlTextBox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Boy name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Girl Name";
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(119, 226);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 53);
+            this.clearButton.TabIndex = 7;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // nameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 343);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.girlTextBox);
+            this.Controls.Add(this.boyTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.displayButton);
+            this.Name = "nameForm";
+            this.Text = "Name Search";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button displayButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox boyTextBox;
+        private System.Windows.Forms.TextBox girlTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ M2HW3_Schweikart/M2HW3_Schweikart/Name Search/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Name_Search
+{
+    public partial class nameForm : Form
+    {
+        public nameForm()
+        {
+            InitializeComponent();
+        }
+
+        string[] bNames = new string[200];
+        string[] gNames = new string[200];
+        int i = 0;
+          
+        private void displayButton_Click(object sender, EventArgs e)
+        {
+            string b = boyTextBox.Text;
+            string g = girlTextBox.Text;
+            
+            int count = 0;
+            bool found = false;
+            
+            StreamReader boyNames = new StreamReader("BoyNames.txt");
+            StreamReader girlNames = new StreamReader("GirlNames.txt");
+            
+
+
+            try
+            {
+
+                 
+                while(boyNames.EndOfStream && count < bNames.Length)
+                {
+                    bNames[count] = boyNames.ReadLine();
+                    count++;
+                    break;
+                }
+                while(girlNames.EndOfStream && count < gNames.Length)
+                {
+                    gNames[count] = girlNames.ReadLine();
+                    count++;
+                    break;
+                }
+                
+
+                if(b != "" && bNames != null && g != "" && gNames != null)
+                {
+                    string bSearchName = boyNames.ReadLine();
+                    string gSearchName = girlNames.ReadLine();
+
+                    while (!found)
+                    {
+                        if(bSearchName == b)
+                        {
+                            found = true;
+                            break;
+                        }
+                       
+                        else if (gSearchName == g)
+                        {
+                            found = true;
+                            break;
+                        }
+                        i++;
+                        
+                    }
+
+                    if (found)
+                    {
+                        MessageBox.Show("The name " + bSearchName + " and the name " + gSearchName + " was found ");
+                    }
+                    
+                    
+                }
+            }
+
+            catch // if an error happens display message
+            {
+                MessageBox.Show("Another Error");
+            }
+        }
+
+        //Clear textboxes
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            boyTextBox.Clear();
+            girlTextBox.Clear();
+        }
+
+        //Close the form
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
